3c5bdaff4622e9e629b39f089ce483f7011c5746
Jan Bednář <mail@janbednar.eu>
2020-10-14 19:01:27 +0200

    Polish

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,34 @@
 #!/usr/bin/env node
 
-import {ExecOptions} from "@actions/exec/lib/interfaces";
+import { ExecOptions } from "@actions/exec/lib/interfaces";
 import core = require("@actions/core");
 import github = require("@actions/github");
 import exec = require("@actions/exec");
 import grok = require("grok-js");
 import * as Context from "@actions/github/lib/context";
-import {GitHub} from "@actions/github/lib/utils";
+import { GitHub } from "@actions/github/lib/utils";
 
 const {
     GITHUB_ACTOR,
     GITHUB_WORKSPACE,
     HOME,
     GITHUB_REPOSITORY,
-    GITHUB_RUN_ID
+    GITHUB_RUN_ID,
 } = process.env;
 const TOKEN: string = core.getInput("token", { required: true });
 const client: InstanceType<typeof GitHub> = github.getOctokit(TOKEN);
 const context: Context.Context = github.context;
 const { owner, repo } = context.repo;
+const { issues, actions, reactions } = client;
 const WORKFLOW_URL: string = `https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}`;
 const patterns: grok.GrokCollection = grok.loadDefaultSync();
 
 async function run() {
     const command: string = core.getInput("command", { required: true });
-    const cancel: boolean = core.getInput("cancel", { required: false }) === "true";
-    const reply: boolean = core.getInput("reply", { required: false }) === "true";
-    const role: string = core.getInput("role", { required: false });
-    const checkout: boolean = core.getInput("checkout", { required: false }) === "true";
+    const cancel: boolean = core.getInput("cancel") === "true";
+    const reply: boolean = core.getInput("reply") === "true";
+    const role: string = core.getInput("role");
+    const checkout: boolean = core.getInput("checkout") === "true";
 
     if (
         context.eventName === "issue_comment" &&
@@ -36,7 +37,9 @@ async function run() {
         core.info("Not a pull request comment");
         core.setOutput("triggered", false);
         if (cancel) {
-            await cancelSelf();
+            await cancelSelf().catch((reason) =>
+                core.setFailed(`Cancel failed: ${reason}`)
+            );
         }
         return;
     }
@@ -49,32 +52,34 @@ async function run() {
     if (!messageMatchPattern(body, command)) {
         core.info(`Comment does not match pattern ${command}`);
         if (cancel) {
-            await cancelSelf();
+            await cancelSelf().catch((reason) =>
+                core.setFailed(`Cancel failed: ${reason}`)
+            );
         }
         return;
     }
 
-    if (role !== "ALL" && !(await commenterHasRole(role))) {
+    if (role !== "ALL" && !commenterHasRole(role)) {
         await comment(
             `Role ${role} is required to execute command.`
-        ).catch(_ => {});
+        ).catch((_) => {});
         core.setOutput("triggered", false);
         return;
     }
 
     core.setOutput("triggered", true);
-    await react("+1").catch(reason =>
+    await react("+1").catch((reason) =>
         core.setFailed(`React failed failed: ${reason}`)
     );
 
     if (reply) {
-        await comment(`Executed workflow: ${WORKFLOW_URL}`).catch(reason =>
+        await comment(`Executed workflow: ${WORKFLOW_URL}`).catch((reason) =>
             core.setFailed(`Reply failed: ${reason}`)
         );
     }
 
     if (checkout) {
-        await checkoutBranch().catch(reason =>
+        await checkoutBranch().catch((reason) =>
             core.setFailed(`Checkout failed: ${reason}`)
         );
     }
@@ -96,54 +101,58 @@ function messageMatchPattern(message: string, messagePattern: string) {
 
 async function react(reaction: any) {
     if (context.eventName === "issue_comment") {
-        await client.reactions.createForIssueComment({
+        await reactions.createForIssueComment({
             owner,
             repo,
             comment_id: context!.payload!.comment!.id,
-            content: reaction
+            content: reaction,
         });
     } else {
-        await client.reactions.createForIssue({
+        await reactions.createForIssue({
             owner,
             repo,
             issue_number: context!.payload!.issue!.number,
-            content: reaction
+            content: reaction,
         });
     }
 }
 
 async function comment(message: string) {
-    await client.issues.createComment({
+    await issues.createComment({
         owner,
         repo,
         issue_number: context!.payload!.issue!.number!,
-        body: `@${GITHUB_ACTOR} ${message}`
+        body: `@${GITHUB_ACTOR} ${message}`,
     });
 }
 
 async function checkoutBranch() {
     const options: ExecOptions = {};
-    options['listeners'] = {
-        stdout: data => {
+    options["listeners"] = {
+        stdout: (data) => {
             core.info(data.toString());
         },
-        stderr: data => {
-            core.warning(data.toString());
-        }
+        stderr: (data) => {
+            console.warn(data.toString());
+        },
     };
-    options.env = options.env || {};
-    options.env["GITHUB_TOKEN"] = TOKEN;
-    options.env["GITHUB_USER"] = GITHUB_ACTOR!;
-    options.env["HUB_PROTOCOL"] = "https";
-    options.env["GITHUB_REPOSITORY"] = context!.payload!.repository!.full_name!;
-    options.env["HOME"] = HOME || '.';
-    options.env["PATH"] =
-        "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
+    options.env = {
+        GITHUB_TOKEN: TOKEN,
+        GITHUB_USER: GITHUB_ACTOR!,
+        HUB_PROTOCOL: "https",
+        GITHUB_REPOSITORY: context!.payload!.repository!.full_name!,
+        HOME: HOME || ".",
+        PATH: "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+    };
     options.cwd = GITHUB_WORKSPACE;
     const pr_number = context?.payload?.issue?.number;
     await exec.exec(
         "hub",
-        ["clone", context!.payload!.repository!.full_name!, GITHUB_WORKSPACE || '.'],
+        [
+            "clone",
+            context!.payload!.repository!.full_name!,
+            GITHUB_WORKSPACE || ".",
+        ],
         options
     );
     await exec.exec(
@@ -155,10 +164,10 @@ async function checkoutBranch() {
 }
 
 async function cancelSelf() {
-    await client.actions.cancelWorkflowRun({
+    await actions.cancelWorkflowRun({
         owner: owner,
         repo: repo,
-        run_id: Number(GITHUB_RUN_ID)
+        run_id: Number(GITHUB_RUN_ID),
     });
 }
 
@@ -166,7 +175,7 @@ function commenterHasRole(role: string): boolean {
     return context!.payload!.comment!.get("author_association") === role;
 }
 
-run().catch(err => {
+run().catch((err) => {
     core.error(err);
     core.setFailed("Unexpected error");
 });
